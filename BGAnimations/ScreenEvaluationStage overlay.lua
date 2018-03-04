4efe212f4197f1026536bb44a1c26e36deede48c
dougshell <omegatron1@gmail.com>
2018-03-04 14:07:58 -0500

    Merge Clock Banner to various screens
    This commit merges a feature from Horsey that allows a Date and Time function to various screens,
    
    These screens can be whitelisted in Home Mode via edits at line 144 in BGAnimations\ScreenSystemLayer overlay.lua

--- BGAnimations/ScreenEvaluationStage overlay.lua
+++ BGAnimations/ScreenEvaluationStage overlay.lua
@@ -1,4 +1,18 @@
 return Def.ActorFrame{
 	-- the content
-	LoadActor( THEME:GetPathB("ScreenEvaluation","common") )
+	LoadActor( THEME:GetPathB("ScreenEvaluation","common") ),
+
+	LoadFont("_wendy small")..{
+		InitCommand=cmd(zoom,WideScale(0.3, 0.4); xy, _screen.cx, SCREEN_BOTTOM-15; horizalign,center; diffusealpha,0; queuecommand,"TextSet"),
+		TextSetCommand=function(self)
+			self:settext(string.format('%2i:%02i %s %02i, %04i', Hour(), Minute(), MonthToString(MonthOfYear()), DayOfMonth(), Year()))
+		end,
+		OnCommand=cmd(decelerate,0.5; diffusealpha,1),
+		OffCommand=cmd(accelerate,0.5;diffusealpha,0),
+		PulseMessageCommand=cmd(playcommand,"TextSet")
+	},
+	Def.Quad {
+		PulseCommand=function(self) MESSAGEMAN:Broadcast("Pulse"); self:sleep(1); self:queuecommand("Pulse"); end;
+		InitCommand=cmd(visible,false;playcommand,"Pulse");
+	}
 }
