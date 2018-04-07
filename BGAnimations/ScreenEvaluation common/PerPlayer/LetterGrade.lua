41d75b9d4d0d2ca935d4f16e2b50cbf21be2a17a
Sangyeol Baek <sangyeolbaek@wirelessprv-10-192-77-130.near.illinois.edu>
2018-04-06 21:48:22 -0500

    Revert "add glowing grades Please enter the commit message for your changes. Lines starting"
    This reverts commit 2c7dc3a8fa8b09ecd24d039c65b88ffca89b2103.
    
    Conflicts:
    	BGAnimations/ScreenEvaluation common/PerPlayer/LetterGrade.lua

--- BGAnimations/ScreenEvaluation common/PerPlayer/LetterGrade.lua
+++ BGAnimations/ScreenEvaluation common/PerPlayer/LetterGrade.lua
@@ -8,6 +8,7 @@ if SL.Global.GameMode ~= "StomperZ" then
 	local title = GAMESTATE:GetCurrentSong():GetDisplayFullTitle()
 	if title == "D" then grade = "Grade_Tier99" end
 
+<<<<<<< HEAD
 	return LoadActor(THEME:GetPathG("", "_grades/"..grade..".lua"), playerStats)..{
 		InitCommand=cmd(xy, 70, _screen.cy-134),
 		OnCommand=function(self)
@@ -16,5 +17,24 @@ if SL.Global.GameMode ~= "StomperZ" then
 				self:x( self:GetX() * -1 )
 			end
 		end
+=======
+	local t = Def.ActorFrame{
+
+		LoadActor(THEME:GetPathG("", "_grades/"..grade..".lua"), playerStats)..{
+			InitCommand=cmd(xy, 70, _screen.cy-134),
+			OnCommand=function(self)
+				self:zoom(0.4)
+				if pn == PLAYER_1 then
+					self:x( self:GetX() * -1 )
+				end
+			end
+		},
+
+	    LoadActor("nice.lua",pn)
+
+>>>>>>> parent of 2c7dc3a... add glowing grades Please enter the commit message for your changes. Lines starting
 	}
+
+	return t
+
 end
